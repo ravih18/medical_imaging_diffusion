5491284f0d8cb4174ee6be8a3463864d40db25f2
ravih18 <rhassana96@gmail.com>
2025-01-24 17:19:55 +0100

    main training code for dsb

---
+++ main_dsb.py
@@ -0,0 +1,25 @@
+import toml
+from pathlib import Path
+import argparse
+
+from utils.config import *
+import torch
+from diffusion.dsb import DiffusionSchrodingerBridge
+
+parser = argparse.ArgumentParser(description='Training DSB model')
+parser.add_argument(
+    'model_number',
+    help='Folder containing the toml config',
+)
+args = parser.parse_args()
+
+caps_dir = Path("/lustre/fswork/projects/rech/krk/commun/datasets/IXI/caps_IXI")
+expe_dir = Path(f"experiments/dsb{args.model_number}")
+
+print(f"Working dir : {expe_dir}")
+
+transfer = True
+dsb = DiffusionSchrodingerBridge(caps_dir, expe_dir, transfer)
+print(dsb.device)
+
+dsb.train()
